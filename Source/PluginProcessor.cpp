969b349cb84c57cc6a7212a2bb2ac665d1ed7c02
dariush-derakhshani <93632084+dariush-derakhshani@users.noreply.github.com>
2023-03-20 01:59:54 +0330

    Karl's suggestions
    CMakeLists and functions updated

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -2,162 +2,130 @@
 // Code by Dariush Derakhshani
 
 #include "PluginProcessor.h"
-#include "PluginEditor.h"
 #include <JuceHeader.h>
+#include "PluginEditor.h"
 
-VintageVibeProcessor::VintageVibeProcessor()
-{
+VintageVibeProcessor::VintageVibeProcessor() {}
 
+VintageVibeProcessor::~VintageVibeProcessor() { freqShifts.clear(); }
+
+void VintageVibeProcessor::prepareToPlay(double sampleRate,
+                                         int samplesPerBlock) {
+  const int totalNumInputChannels = getTotalNumInputChannels();
+
+  freqShifts.clear();
+
+  userDefinedFrequencyShift = 50.0f / sampleRate;
+
+  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
+    freqShifts.add(new gam::FreqShift<float>(5.0f));
+    gam::FreqShift<float>& freqshift = *freqShifts.getUnchecked(channel);
+    freqshift.freq(5.0f / sampleRate);
+  }
+  onepole[0].freq(10);
+  onepole[1].freq(10);
 }
 
-VintageVibeProcessor::~VintageVibeProcessor()
-{
-    freqShifts.clear();
+void VintageVibeProcessor::releaseResources() {}
+
+int VintageVibeProcessor::getNumPrograms() { return 1; }
+
+int VintageVibeProcessor::getCurrentProgram() { return 0; }
+
+void VintageVibeProcessor::setCurrentProgram(int index) {}
+
+const juce::String VintageVibeProcessor::getProgramName(int index) {
+  return {};
 }
 
-void VintageVibeProcessor::prepareToPlay(double sampleRate, int samplesPerBlock)
-{
-    const int totalNumInputChannels = getTotalNumInputChannels();
+void VintageVibeProcessor::changeProgramName(int index,
+                                             const juce::String& newName) {}
 
-    freqShifts.clear();
+void VintageVibeProcessor::getStateInformation(juce::MemoryBlock& destData) {}
 
-    userDefinedFrequencyShift = 50.0f / sampleRate;
+void VintageVibeProcessor::setStateInformation(const void* data,
+                                               int sizeInBytes) {}
 
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        freqShifts.add(new gam::FreqShift<float>(5.0f));
-        gam::FreqShift<float>& freqshift = *freqShifts.getUnchecked(channel);
-        freqshift.freq(5.0f / sampleRate);
-    }
+bool VintageVibeProcessor::acceptsMidi() const { return false; }
+
+bool VintageVibeProcessor::producesMidi() const { return false; }
+
+bool VintageVibeProcessor::isMidiEffect() const { return false; }
+
+double VintageVibeProcessor::getTailLengthSeconds() const { return 0.0; }
+
+void VintageVibeProcessor::setGain(float amount) { gain = amount; }
+
+void VintageVibeProcessor::randomFrequencyShift() {
+  float minFactor = 0.5f;
+  float maxFactor = 1.5f;
+  float randomFactor =
+      juce::Random::getSystemRandom().nextFloat() * (maxFactor - minFactor) +
+      minFactor;
+
+  float modulatedFrequency = userDefinedFrequencyShift * randomFactor;
+  setFrequencyShiftAmount(modulatedFrequency);
+  printf("%f\n", frequencyShiftAmount + randomFactor);
 }
 
-void VintageVibeProcessor::releaseResources()
-{
+void VintageVibeProcessor::setFrequencyShiftAmount(float amount) {
+  frequencyShiftAmount = amount;
+
+  for (int channel = 0; channel < getTotalNumInputChannels(); ++channel) {
+    float modulatedFrequencyShiftAmount =
+        frequencyShiftAmount / getSampleRate();
+    gam::FreqShift<float>& freqshift = *freqShifts.getUnchecked(channel);
+    freqshift.freq(modulatedFrequencyShiftAmount);
+  }
 }
 
-int VintageVibeProcessor::getNumPrograms()
-{
-return 1;
+void VintageVibeProcessor::setUserDefinedFrequencyShift(float amount) {
+  userDefinedFrequencyShift = amount;
 }
 
-int VintageVibeProcessor::getCurrentProgram()
-{
-return 0;
-}
-
-void VintageVibeProcessor::setCurrentProgram(int index)
-{
-}
-
-const juce::String VintageVibeProcessor::getProgramName(int index)
-{
-return {};
-}
-
-void VintageVibeProcessor::changeProgramName(int index, const juce::String& newName)
-{
-}
-
-void VintageVibeProcessor::getStateInformation(juce::MemoryBlock& destData)
-{
-}
-
-void VintageVibeProcessor::setStateInformation(const void* data, int sizeInBytes)
-{
-}
-
-bool VintageVibeProcessor::acceptsMidi() const
-{
-    return false;
-}
-
-bool VintageVibeProcessor::producesMidi() const
-{
-
-    return false;
-}
-
-bool VintageVibeProcessor::isMidiEffect() const
-{
-    return false;
-}
-
-double VintageVibeProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-void VintageVibeProcessor::setGain(float amount)
-{
-    gain = amount;
-}
-
-void VintageVibeProcessor::randomFrequencyShift()
-{
+void VintageVibeProcessor::processBlock(juce::AudioBuffer<float>& buffer,
+                                        juce::MidiBuffer& midiMessages) {
+  float modulatedFrequencyShiftAmount[2];
+  for (int channel = 0; channel < getTotalNumInputChannels(); ++channel) {
     float minFactor = 0.5f;
     float maxFactor = 1.5f;
-    float randomFactor = juce::Random::getSystemRandom().nextFloat() * (maxFactor - minFactor) + minFactor;
+    float randomFactor =
+        juce::Random::getSystemRandom().nextFloat() * (maxFactor - minFactor) +
+        minFactor;
 
     float modulatedFrequency = userDefinedFrequencyShift * randomFactor;
-    setFrequencyShiftAmount(modulatedFrequency);
-    printf("%f\n", frequencyShiftAmount + randomFactor);
-}
+    modulatedFrequencyShiftAmount[channel] =
+        frequencyShiftAmount / getSampleRate();
+  }
 
-void VintageVibeProcessor::setFrequencyShiftAmount(float amount)
-{
-    frequencyShiftAmount = amount;
+  const int totalNumInputChannels = getTotalNumInputChannels();
 
-    for (int channel = 0; channel < getTotalNumInputChannels(); ++channel)
-    {
-        float modulatedFrequencyShiftAmount = frequencyShiftAmount / getSampleRate();
-        gam::FreqShift<float>& freqshift = *freqShifts.getUnchecked(channel);
-        freqshift.freq(modulatedFrequencyShiftAmount);
+  jassert(freqShifts.size() == getTotalNumInputChannels());
+
+  for (int channel = 0; channel < totalNumInputChannels; ++channel) {
+    auto* channelData = buffer.getWritePointer(channel);
+    gam::FreqShift<float>& freqshift = *freqShifts.getUnchecked(channel);
+
+    for (int i = 0; i < buffer.getNumSamples(); ++i) {
+      freqshift.freq(onepole[channel](modulatedFrequencyShiftAmount[channel]));
+      const float freqshiftValue = freqshift(channelData[i]);
+      channelData[i] = freqshiftValue * gain;
     }
+  }
 }
 
-void VintageVibeProcessor::setUserDefinedFrequencyShift(float amount)
-{
-    userDefinedFrequencyShift = amount;
+juce::AudioProcessorEditor* VintageVibeProcessor::createEditor() {
+  return new VintageVibeEditor(*this);
 }
 
+bool VintageVibeProcessor::hasEditor() const { return true; }
 
-void VintageVibeProcessor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
-{
-    randomFrequencyShift();
-
-    const int totalNumInputChannels = getTotalNumInputChannels();
-
-    jassert(freqShifts.size() == getTotalNumInputChannels());
-
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer(channel);
-        gam::FreqShift<float>& freqshift = *freqShifts.getUnchecked(channel);
-
-        for (int i = 0; i < buffer.getNumSamples(); ++i)
-        {
-            const float freqshiftValue = freqshift(channelData[i]);
-            channelData[i] = freqshiftValue * gain;
-        }
-    }
-}
-
-juce::AudioProcessorEditor* VintageVibeProcessor::createEditor()
-{
-    return new VintageVibeEditor (*this);
-}
-
-bool VintageVibeProcessor::hasEditor() const
-{
-    return true;
-}
-
-const juce::String VintageVibeProcessor::getName() const
-{
-    return JucePlugin_Name;
+const juce::String VintageVibeProcessor::getName() const {
+  return JucePlugin_Name;
 }
 
 // this is where I'm receiving and error:
 
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{ return new VintageVibeProcessor(); }
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() {
+  return new VintageVibeProcessor();
+}
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -15,7 +15,7 @@ public:
     ~VintageVibeProcessor() override;
     
     static juce::AudioProcessor* createPluginFilter();
-
+    
     void prepareToPlay(double sampleRate, int samplesPerBlock) override;
     
     void releaseResources() override;
@@ -24,29 +24,30 @@ public:
     bool producesMidi() const override;
     bool isMidiEffect() const override;
     double getTailLengthSeconds() const override;
-
+    
     int getNumPrograms() override;
     int getCurrentProgram() override;
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
     void changeProgramName (int index, const juce::String& newName) override;
-
+    
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
-
+    
     void setGain(float amount);
     void setFrequencyShiftAmount(float amount);
     void setUserDefinedFrequencyShift(float amount);
-
+    
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
-
+    
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
-
+    
     const juce::String getName() const override;
-
+    
 private:
     juce::OwnedArray<gam::FreqShift<>> freqShifts;
+    gam::OnePole<> onepole[2];
     float gain = 1.0f;
     float frequencyShiftAmount = 0.0f;
     float userDefinedFrequencyShift = 0.0f;
